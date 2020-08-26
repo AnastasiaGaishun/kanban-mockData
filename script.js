e596833fc011fbe4d5f0dc68f3f24851c2e72599
AnastasiaGaishun <63657742+AnastasiaGaishun@users.noreply.github.com>
2020-08-26 21:45:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-// const mochResponse = {
-//     status: 'success'
-// };
+const appElement = document.querySelector('.app');
+
+let htmlString = '';
+
+for (let index = 0; index < GET_COLUMNS_RESPONSE.length; index++) {
+
+    const colmnId = GET_COLUMNS_RESPONSE[index].id;
+    const currentColumnCards = getCardsByColumnId(colmnId);
+    let allCardsForColumnHtml = '';
+
+    currentColumnCards.forEach(function (x) {
+        allCardsForColumnHtml += getCard(x.title, x.description, x.userId);
+    });
 
 
+    htmlString += `<div class="col">
+                        <div class="col__header">${GET_COLUMNS_RESPONSE[index].name}</div>
+                        <div class="col_content">${allCardsForColumnHtml}</div>
+                    </div>`;
+}
 
+appElement.innerHTML = htmlString;
 
+function getCardsByColumnId(id) {
+    return GET_CARDS_RESPONSE.filter(function (card) {
+        return card.columnId === id;
+    })
+}
 
-
-
-
-
-
-// fetch('mail.ru').then(x =>{
-//  if x.status =
-// });
+function getCard(title, description, userName) {
+    return `<div class="card">
+                <div class="card__inform">
+                    <div class="card__title">${title}</div>
+                    <div class="card__description">${description}</div>
+                </div>
+                <div class="card__user">
+                    <div class="card__user-name">${userName}</div>
+                </div>
+            </div>`;
+}
